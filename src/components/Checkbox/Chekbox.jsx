a4aa193366f50d1f3a8bbbaa17272c68ff52da62
julia953197 <j.andryukova@gmail.com>
2022-07-18 14:09:07 +0300

    Add movies search

--- src/components/Checkbox/Chekbox.jsx
+++ src/components/Checkbox/Chekbox.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./Checkbox.css";
 
-function Checkbox(){
+function Checkbox({checked, onChange}){
+    const [isShort,setIsShort] = React.useState(checked);
+
     function handleChange(){
         const background = document.querySelector(".checkbox__checkmark-background");
         const checker = document.querySelector(".checkbox__checkmark-checker");
         background.classList.toggle("checkbox__checkmark-background_disable");
         checker.classList.toggle("checkbox__checkmark-checker_disable");
+        onChange(!checked);
     }
     return(
         <div className="checkbox__container">
             <label className="checkbox__label">
-                <input type="checkbox" checked="checked" onChange={handleChange} className="checkbox__default"/>
+                <input type="checkbox" checked={checked} onChange={handleChange} className="checkbox__default"/>
                 <div className="checkbox__checkmark">
                     <div className="checkbox__checkmark-background"></div>
                     <div className="checkbox__checkmark-checker"></div>
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -6,7 +6,7 @@ import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import getMovies from "../../utils/MoviesApi";
 import {useMediaPredicate} from "react-media-hook";
 
-function Movies(){
+function Movies() {
     const isPad = useMediaPredicate("(min-width: 481px)");
     const isDesktop = useMediaPredicate("(min-width: 768px)");
     const [allMovies, setAllMovies] = React.useState([]);
@@ -15,60 +15,96 @@ function Movies(){
     const [filteredShortMovies, setFilteredShortMovies] = React.useState([]);
     const [moviesToRender, setMoviesToRender] = React.useState([]);
     const [maxMoviesNumber, setMaxMoviesNumber] = React.useState(setInitialMoviesNumber());
+    const [isShort, setIsShort] = React.useState(true);
 
-    function setInitialMoviesNumber(){
-        if (isDesktop){
+    function setInitialMoviesNumber() {
+        if (isDesktop) {
             return 12;
         }
-        if (isPad){
+        if (isPad) {
             return 8;
         }
         return 5;
     }
 
-    const addMoreFilms = ()=>{
-        if (isDesktop){
+    function addMoreFilms() {
+        if (isDesktop) {
             return 3;
         }
         return 2;
     }
 
-    React.useEffect(()=>{
-        if(allMovies.length === 0){
-            getMovies().then((movies)=>{
-                setAllMovies(movies);
-            })
-                .catch((err)=>{
-                    console.log(err);
-                })
-        }
-    },[])
 
-    function getRenderMovies(movies){
+    function getRenderMovies(movies) {
         const result = [];
-        for (let i = 0; i < maxMoviesNumber && i < movies.length; i =i+1) {
+        for (let i = 0; i < maxMoviesNumber && i < movies.length; i = i + 1) {
             result.push(movies[i]);
         }
         setMoviesToRender(result);
     }
 
-    function renderMovies(){
-        getRenderMovies(allMovies);
+    function renderMovies(movies) {
+        if (movies === undefined) {
+            getRenderMovies(allMovies);
+        } else {
+            getRenderMovies(movies);
+        }
+
     }
+
+    function handleSearch(keyword) {
+        const filterMovies = allMovies.filter((movie) =>
+            movie.nameRU.toLowerCase().indexOf(keyword.toLowerCase()) > -1);
+        const shortFilterMovies = filterMovies.filter((movie) =>
+            movie.duration <= 40);
+        const longFilterMovies = filterMovies.filter((movie) =>
+            movie.duration > 40);
+        setFilteredShortMovies(shortFilterMovies);
+        setFilteredLongMovies(longFilterMovies);
+
+    }
+
+    function handleShortClick() {
+        setIsShort(!isShort);
+    }
+
+
+    React.useEffect(() => {
+        if (allMovies.length === 0) {
+            getMovies().then((movies) => {
+                setAllMovies(movies);
+            })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
+    }, [])
 
     React.useEffect(() => {
         renderMovies();
-    }, [filteredMovies, filteredLongMovies, maxMoviesNumber, allMovies]);
+    }, [allMovies]);
 
-    return(
+    React.useEffect(() => {
+        if (isShort) {
+            console.log("render short");
+            renderMovies(filteredShortMovies)
+        } else {
+            console.log("render long");
+            renderMovies(filteredLongMovies);
+        }
+    }, [maxMoviesNumber, isShort, filteredLongMovies, filteredShortMovies]);
+
+    return (
         <>
             <Header loggedIn={true} isMovies={true}/>
             <main>
-                <SearchForm/>
+                <SearchForm onSearch={handleSearch}
+                            checked={isShort}
+                            onCheckClick={handleShortClick}/>
                 <MoviesCardList
                     isSavedMoviesPage={false}
                     isMoreButton={true}
-                    movies = {moviesToRender}
+                    movies={moviesToRender}
                 />
             </main>
             <Footer/>
