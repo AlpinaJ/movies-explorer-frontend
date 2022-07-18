7e1dbd3bb6480f97f4f0596b4f46d4f0890c69a8
julia953197 <j.andryukova@gmail.com>
2022-07-18 10:41:34 +0300

    Add movies render

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,14 +3,73 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import getMovies from "../../utils/MoviesApi";
+import {useMediaPredicate} from "react-media-hook";
 
 function Movies(){
+    const isPad = useMediaPredicate("(min-width: 481px)");
+    const isDesktop = useMediaPredicate("(min-width: 768px)");
+    const [allMovies, setAllMovies] = React.useState([]);
+    const [filteredMovies, setFilteredMovies] = React.useState([]);
+    const [filteredLongMovies, setFilteredLongMovies] = React.useState([]);
+    const [filteredShortMovies, setFilteredShortMovies] = React.useState([]);
+    const [moviesToRender, setMoviesToRender] = React.useState([]);
+    const [maxMoviesNumber, setMaxMoviesNumber] = React.useState(setInitialMoviesNumber());
+
+    function setInitialMoviesNumber(){
+        if (isDesktop){
+            return 12;
+        }
+        if (isPad){
+            return 8;
+        }
+        return 5;
+    }
+
+    const addMoreFilms = ()=>{
+        if (isDesktop){
+            return 3;
+        }
+        return 2;
+    }
+
+    React.useEffect(()=>{
+        if(allMovies.length === 0){
+            getMovies().then((movies)=>{
+                setAllMovies(movies);
+            })
+                .catch((err)=>{
+                    console.log(err);
+                })
+        }
+    },[])
+
+    function getRenderMovies(movies){
+        const result = [];
+        for (let i = 0; i < maxMoviesNumber && i < movies.length; i =i+1) {
+            result.push(movies[i]);
+        }
+        setMoviesToRender(result);
+    }
+
+    function renderMovies(){
+        getRenderMovies(allMovies);
+    }
+
+    React.useEffect(() => {
+        renderMovies();
+    }, [filteredMovies, filteredLongMovies, maxMoviesNumber, allMovies]);
+
     return(
         <>
             <Header loggedIn={true} isMovies={true}/>
             <main>
                 <SearchForm/>
-                <MoviesCardList isSavedMoviesPage={false} isMoreButton={true}/>
+                <MoviesCardList
+                    isSavedMoviesPage={false}
+                    isMoreButton={true}
+                    movies = {moviesToRender}
+                />
             </main>
             <Footer/>
         </>
--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./MoviesCard.css";
 
-function MoviesCard({name, image, duration, isSaved, isInSavedMovies}) {
+function MoviesCard({movie, isSaved, isInSavedMovies}) {
     return (
         <div className="card">
-            <img src={image} alt={name}/>
+            <img src={`https://api.nomoreparties.co${movie.image.url}`}
+                 alt={movie.nameRU} className="card__image"/>
             <div className="card__info">
-                <h4 className="card__name">{name}</h4>
-                <p className="card__duration">{duration}</p>
+                <h4 className="card__name">{movie.nameRU}</h4>
+                <p className="card__duration">{movie.duration}</p>
                 {isInSavedMovies ?
                     (<button type="button" className="card__button card__button_delete"></button>)
                     :(isSaved ?
