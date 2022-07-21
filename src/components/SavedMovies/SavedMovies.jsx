7d7617a4c56e378c6a10554ca15c21cc63f28720
julia953197 <j.andryukova@gmail.com>
2022-07-21 06:53:00 +0300

    Add search in saved-movies

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -7,15 +7,21 @@ import {stringToBool} from "../../utils/constants";
 
 function SavedMovies({handleDelete, movies}){
     const [isShort, setIsShort] = React.useState(stringToBool(localStorage.getItem('isShort')));
+    const initialSavedMovies = isShort ?
+        (movies.filter((movie)=>movie.duration<=40)):
+        (movies.filter((movie)=>movie.duration>40));
+    const [moviesToRender, setMoviesToRender]=React.useState(initialSavedMovies);
 
     function handleSearch(keyword){
         if(isShort){
-            return movies.filter((movie) => movie.nameRU.toLowerCase().indexOf(keyword.toLowerCase()) > -1
-            && movie.duration<=40);
+            let result =  movies.filter((movie) => movie.nameRU.toLowerCase().indexOf(keyword.toLowerCase()) > -1);
+            result = result.filter((movie) => movie.duration>40);
+            setMoviesToRender(result);
         }
         else{
-            return movies.filter((movie) => movie.nameRU.toLowerCase().indexOf(keyword.toLowerCase()) > -1
-                && movie.duration>40);
+            let result =  movies.filter((movie) => movie.nameRU.toLowerCase().indexOf(keyword.toLowerCase()) > -1);
+            result= result.filter((movie) => movie.duration<=40);
+            setMoviesToRender(result);
         }
 
     }
@@ -23,6 +29,7 @@ function SavedMovies({handleDelete, movies}){
     function handleShortClick() {
         localStorage.setItem("isShort", String(!isShort));
         setIsShort(!isShort);
+        handleSearch(localStorage.getItem("keyword"));
     }
 
     return(
@@ -38,7 +45,7 @@ function SavedMovies({handleDelete, movies}){
                     isSavedMoviesPage={true}
                     isMoreButton={false}
                     savedMovies={movies}
-                    movies={isShort?(movies.filter((movie)=>movie.duration<=40)):(movies.filter((movie)=>movie.duration>40))}
+                    movies={moviesToRender}
                     handleClick={handleDelete}
                 />
             </main>
